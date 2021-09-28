0ebef1b7ee7a549312f84c87a8dac675700c3df4
shiido <jhonfr29@gmail.com>
2021-09-28 16:43:12 -0500

    feat: new category task added

--- src/main/java/com/ai/st/microservice/tasks/business/TaskCategoryBusiness.java
+++ src/main/java/com/ai/st/microservice/tasks/business/TaskCategoryBusiness.java
@@ -7,5 +7,6 @@ public class TaskCategoryBusiness {
 
 	public static final Long TASK_CATEGORY_INTEGRATION = (long) 1;
 	public static final Long TASK_CATEGORY_CADASTRAL_INPUT_GENERATION = (long) 2;
+	public static final Long TASK_CATEGORY_XTF_QUALITY_RULES = (long) 3;
 
 }
